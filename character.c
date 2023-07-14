eecb242970398ea4d7dac860c11b9cf7b7ceda09
bl4ck5 <114998095+webRemi@users.noreply.github.com>
2023-07-14 20:16:48 +0200

    Add files via upload
    perfect move for pacman character

--- character.c
+++ character.c
@@ -2,39 +2,57 @@
 
 int main()
 {
+    // initialize screen
     initscr();
     cbreak();
+    // remove echos
     noecho();
+    // take input from keyboard
     keypad(stdscr, TRUE);
+    // initilize lines and colonnes
     int x = 0;
     int y = 0;
+    // initialize key
     int key;
+    // set shape for cursor (0, 1, 2)
+    curs_set(0);
     
     while (1)
     {
+        // mv pacman to position updated
         mvaddch(y, x, '<');
+        // refresh screen to view updates
         refresh();
+        
+        // take user input (for arrow keys)
         key = getch();
+        // check position where pacman need to move
         switch(key)
         {
             case KEY_UP:
+            // update position with blank value (to hide old position)
+            mvaddch(y, x, ' ');
+            refresh();
             y--;
             break;
             
             case KEY_RIGHT:
+            mvaddch(y, x, ' ');
             x++;
             break;
 
             case KEY_DOWN:
+            mvaddch(y, x, ' ');
             y++;
             break;
 
             case KEY_LEFT:
+            mvaddch(y, x, ' ');
             x--;
             break;
         }
     }
-
+    // restore terminal
     endwin();
 
     return 0;
