d2372e714c282a44ed9ccf784d10b5ec7206f0ff
bl4ck5 <114998095+webRemi@users.noreply.github.com>
2023-07-14 19:42:32 +0200

    Add files via upload
    move feature

--- character.c
+++ character.c
@@ -3,18 +3,36 @@
 int main()
 {
     initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
     int x = 0;
     int y = 0;
+    int key;
     
     while (1)
     {
-        //clear();
         mvaddch(y, x, '<');
         refresh();
-        /* if (getch() != 410)
-            break; */
-        y++;
-        x++;
+        key = getch();
+        switch(key)
+        {
+            case KEY_UP:
+            y--;
+            break;
+            
+            case KEY_RIGHT:
+            x++;
+            break;
+
+            case KEY_DOWN:
+            y++;
+            break;
+
+            case KEY_LEFT:
+            x--;
+            break;
+        }
     }
 
     endwin();
