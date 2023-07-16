c273d8bb34b872df809a72cafc8f11df8fb13d78
bl4ck5 <114998095+webRemi@users.noreply.github.com>
2023-07-16 21:55:38 +0200

    Add files via upload
    floor3 added

--- main.c
+++ main.c
@@ -38,25 +38,64 @@ void drawWalls()
 
 void drawInternalWalls()
 {
-    int y = 7;
-    int x = 4;
+    int floor1y = 6;
+    int floor1x = 38;
+    int floor2y = 7;
+    int floor2x = 4;
+    int floor3y = 8;
+    int floor3x = 5;
+    int floor4y = 9;
+    int floor4x = 5;
+    
     initscr();
     start_color();
     attron(COLOR_PAIR(3));
-    while (x < 7)
+    // floor 1
+    mvaddch(floor1y, floor1x, ' ');
+    floor1x++;
+    mvaddch(floor1y, floor1x, ' ');
+    floor1x++;
+    mvaddch(floor1y, floor1x, ' ');
+    floor1x++;
+    mvaddch(floor1y, floor1x, ' ');
+    // floor2
+    while (floor2x < 74)
     {
-        mvaddch(y, ++x, ' ');
-    }
-    while (x < 74)
-    {
-        if (x == 8 || x == 15 || x == 30 || x == 36 || x == 37 || x == 38 || x == 45 || x == 61 || x == 68)
+        if (floor2x == 8 || floor2x == 15 || floor2x == 29 || floor2x == 35 || floor2x == 41 || floor2x == 47 || floor2x == 61 || floor2x == 68 || floor2x == 74)
         {
-            x += 2;
+            floor2x += 2;
             continue;
         }
-        mvaddch(y, ++x, ' ');
+        mvaddch(floor2y, ++floor2x, ' ');
     }
+    // floor3
+    mvaddch(floor3y, floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    floor3x = 12;
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, floor3x += 3, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    floor3x = 21;
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    floor3x = 55;
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    floor3x = 59;
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    floor3x = 63;
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
+    floor3x = 72;
+    mvaddch(floor3y, ++floor3x, ' ');
+    mvaddch(floor3y, ++floor3x, ' ');
     refresh();
+
+    // floor4
     attroff(COLOR_PAIR(3));
 }
 
